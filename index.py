a82797c52130056e08bc93ac6cc63385fd5cb70a
Kannan-Selvaraj-45 <kannanselvaraj4533@gmail.com>
2025-03-19 10:21:34 +0530

    initial

---
+++ index.py
@@ -0,0 +1,27 @@
+from flask import Flask
+import pymysql
+from pymysql import Error
+
+# Establish the database connection using PyMySQL
+mydb = pymysql.connect(
+    host="localhost",
+    user="root",
+    passwd="kannan_45",
+    charset="utf8",
+    database="chain_of_custody"
+)
+
+# Initialize Flask app
+app = Flask(__name__)
+
+@app.route('/test-db')
+def test_db_connection():
+    try:
+        if mydb.open:
+            return "Database connection successful!"
+    except Error as e:
+        return f"Error connecting to the database: {e}"
+
+if __name__ == "__main__":
+    print("Starting Flask app...")
+    app.run(debug=True)
